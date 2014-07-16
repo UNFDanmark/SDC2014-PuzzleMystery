adf4cbf78fa50589ba41e08d9ba130fd4f7e075c
sdc <github@software.unf.dk>
2014-07-16 09:37:38 +0200

    "isen" smelter langsomt i free the princess, og finisher når man vinder

--- src/dk/sdc/PuzzleMystery/FreeThePrincessActivity.java
+++ src/dk/sdc/PuzzleMystery/FreeThePrincessActivity.java
@@ -10,8 +10,8 @@ import android.widget.ImageButton;
  * Created by sdc on 7/15/14.
  */
 public class FreeThePrincessActivity extends Activity {
-    private int winClicks = 10;
-    private int Tællertilis = winClicks;
+    private int winClicks = 25;
+    private int Tællertilis = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,12 +21,14 @@ public class FreeThePrincessActivity extends Activity {
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Tællertilis--;
-                //int transparency = 255*(Tællertilis/winClicks);
-                //myButton.setBackgroundColor(Color.argb(100,77,148,255));
-                if (Tællertilis <= 0) {
+                Tællertilis++;
+                double jump = 255/winClicks;
+                int transparency = (int) (255-(Tællertilis*jump));
+                myButton.setBackgroundColor(Color.argb(transparency,77,148,255));
+                if (Tællertilis >= winClicks) {
                     //myButton.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.princess_theme_av2));
                     myButton.setBackgroundColor(Color.argb(0, 0, 0, 0));
+                    finish();
                 }
             }
         });
